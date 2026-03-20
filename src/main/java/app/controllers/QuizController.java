0deddbcabb8b2fb6977496bace55c68d75b30e81
KatarinaKN <katarinak.nielsen@gmail.com>
2026-03-20 11:04:31 +0100

    Oprettet QuizController med tomme metoder

---
+++ src/main/java/app/controllers/QuizController.java
@@ -0,0 +1,24 @@
+package app.controllers;
+
+public class QuizController {
+
+    /*statiske metoder gør koden sværere at teste, og de passer ikke så godt ind i
+    * objektorienteret tankegang. Derfor er metoderne ikke statiske, og vi skal så huske
+    * at oprette en instans af controlleren i Main.*/
+
+    public void addRoutes() {
+
+    }
+
+    public void showQuestion() {
+
+    }
+
+    public void checkAnswer() {
+
+    }
+
+    public void leaveQuiz() {
+
+    }
+}
