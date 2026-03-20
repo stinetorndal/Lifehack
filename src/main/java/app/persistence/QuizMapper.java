4eb15cee50d84a40969a4bfbabc4a59a5da70191
KatarinaKN <katarinak.nielsen@gmail.com>
2026-03-20 11:08:57 +0100

    Oprettet QuizMapper med tomme metoder

---
+++ src/main/java/app/persistence/QuizMapper.java
@@ -0,0 +1,17 @@
+package app.persistence;
+
+public class QuizMapper {
+
+    /*Måske skal metoderne ikke være void...
+    Metoderne er ikke statiske, så vi skal huske at oprette en instans af
+    mapperen i vores controller.
+     */
+
+    public void getRandomQuestion() {
+
+    }
+
+    public void getAllQuestions() {
+
+    }
+}
